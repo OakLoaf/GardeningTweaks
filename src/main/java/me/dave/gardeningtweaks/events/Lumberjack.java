9168e918006f1ebc9aea3883c46036a8395d9799
BuildTools <cooldaveb@live.co.uk>
2022-10-29 23:11:38 +0100

    First working version of Lumberjack

--- src/main/java/me/dave/gardeningtweaks/events/Lumberjack.java
+++ src/main/java/me/dave/gardeningtweaks/events/Lumberjack.java
@@ -5,6 +5,8 @@ import me.dave.gardeningtweaks.GardeningMode;
 import me.dave.gardeningtweaks.GardeningTweaks;
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -15,12 +17,14 @@ public class Lumberjack implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
+        Material blockType = block.getType();
         Location location = block.getLocation();
 
         // Lumberjack
         ConfigManager.Lumberjack lumberjack = GardeningTweaks.configManager.getLumberjackConfig();
-        if (lumberjack.mode() != GardeningMode.DISABLED && lumberjack.blocks().contains(block.getType())) {
-            breakVeinBlock(block);
+        if (lumberjack.mode() != GardeningMode.DISABLED && lumberjack.blocks().contains(blockType)) {
+            Block blockAbove = block.getRelative(BlockFace.UP);
+            if (blockAbove.getType() == blockType) breakVeinBlock(block);
         }
     }
 
@@ -28,16 +32,20 @@ public class Lumberjack implements Listener {
         Material currType = block.getType();
         Location location = block.getLocation();
         World world = block.getWorld();
+        block.breakNaturally();
+        BlockData blockData = currType.createBlockData();
+        world.playSound(location.clone().add(0.5, 0.5, 0.5), blockData.getSoundGroup().getBreakSound(), 1f, 1f);
+        world.spawnParticle(Particle.BLOCK_DUST, location.clone().add(0.5, 0.5, 0.5), 50, 0.3, 0.3, 0.3, blockData);
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            Location checkLoc = location.clone().add(-1, -1, -1);
             for (int indexY = 1; indexY >= 0; indexY--) {
-                for (int indexX = 1; indexX >= -1; indexX--) {
-                    for (int indexZ = 1; indexZ >= -1; indexZ--) {
+                for (int indexX = -1; indexX <= 1; indexX++) {
+                    for (int indexZ = -1; indexZ <= 1; indexZ++) {
                         if (indexX == 1 && indexY == 1 && indexZ == 1) continue;
-                        Block blockToBreak = checkLoc.clone().add(indexX, indexY, indexZ).getBlock();
-                        if (blockToBreak.getType() == currType) breakVeinBlock(blockToBreak);
-                        blockToBreak.breakNaturally();
-                        world.spawnParticle(Particle.BLOCK_DUST, location.clone().add(0.5, 0.5, 0.5), 50, 0.3, 0.3, 0.3, currType.createBlockData());
+                        Location currLoc = location.clone().add(indexX, indexY, indexZ);
+                        Block blockToBreak = currLoc.getBlock();
+                        if (blockToBreak.getType() == currType) {
+                            breakVeinBlock(blockToBreak);
+                        }
                     }
                 }
             }
@@ -48,10 +56,7 @@ public class Lumberjack implements Listener {
 //        private final GardeningTweaks plugin;
 //        private final Deque<Block> blockStack = new ArrayDeque<>();
 //        private final List<Block> brokenBlocks = new ArrayList<>();
-//        private final Material blockType;
-//        private final BlockData blockData;
-//        private final World world;
-//
+
 //        public LogLumber(GardeningTweaks plugin, Block startBlock) {
 //            this.plugin = plugin;
 //            blockStack.push(startBlock);
@@ -63,36 +68,5 @@ public class Lumberjack implements Listener {
 //        }
 //
 //        private void breakConnectedLogs() {
-//            new BukkitRunnable() {
-//                @Override
-//                public void run() {
-//                    for (int indexY = 1; indexY >= 0; indexY--) {
-//                        for (int indexX = 1; indexX >= -1; indexX--) {
-//                            for (int indexZ = 1; indexZ >= -1; indexZ--) {
-//                                Block nextBlock = world.getBlockAt(blockStack.peek().getLocation().add(indexX, indexY, indexZ));
-//                                if (nextBlock.getType().equals(blockType)) {
-//                                    if (!brokenBlocks.contains(nextBlock)) {
-//                                        world.spawnParticle(Particle.BLOCK_DUST, nextBlock.getLocation().clone().add(0.5, 0.5, 0.5), 50, 0.3, 0.3, 0.3, blockData);
-//                                        world.playSound(nextBlock.getLocation().clone().add(0.5, 0.5, 0.5), blockData.getSoundGroup().getBreakSound(), 1f, 1f);
-//                                        nextBlock.breakNaturally();
-//                                        brokenBlocks.add(nextBlock);
-//                                        if (brokenBlocks.size() >= 31) {
-//                                            cancel();
-//                                            return;
-//                                        }
-//                                    }
-//                                    blockStack.push(nextBlock);
-//                                    return;
-//                                }
-//                            }
-//                        }
-//                    }
-//                    blockStack.pop();
-//                    if (blockStack.isEmpty()) {
-//                        cancel();
-//                    }
-//                }
-//            }.runTaskTimer(plugin, 5, 5);
-//        }
-//    }
+//
 }

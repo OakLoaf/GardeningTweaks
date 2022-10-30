2264dc8f1beb342ab5fd4cdcf7a86306598b8025
BuildTools <cooldaveb@live.co.uk>
2022-10-30 22:07:12 +0000

    Bug fixes: - Set RejuvenatedBushes to right-click only

--- src/main/java/me/dave/gardeningtweaks/events/RejuvenatedBushes.java
+++ src/main/java/me/dave/gardeningtweaks/events/RejuvenatedBushes.java
@@ -8,6 +8,7 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -20,6 +21,7 @@ public class RejuvenatedBushes implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         ConfigManager.RejuvenatedBushes rejuvenatedBushes = GardeningTweaks.configManager.getRejuvenatedBushesConfig();
         if (!rejuvenatedBushes.enabled()) return;
 

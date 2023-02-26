3cb59b760a56e5dc7c07a3f1a8d62c290330227a
BuildTools <cooldaveb@live.co.uk>
2023-02-26 03:30:52 +0000

    Added ignore-persistence option to FastLeafDecay

--- src/main/java/me/dave/gardeningtweaks/events/FastLeafDecay.java
+++ src/main/java/me/dave/gardeningtweaks/events/FastLeafDecay.java
@@ -1,6 +1,6 @@
 package me.dave.gardeningtweaks.events;
 
-import me.dave.gardeningtweaks.ConfigManager;
+import me.dave.gardeningtweaks.datamanager.ConfigManager;
 import me.dave.gardeningtweaks.GardeningTweaks;
 import org.bukkit.*;
 import org.bukkit.block.Block;
@@ -8,26 +8,34 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Leaves;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 public class FastLeafDecay implements Listener {
     private final GardeningTweaks plugin = GardeningTweaks.getInstance();
     private ConfigManager.FastLeafDecay fastLeafDecay;
+    private final NamespacedKey ignoredKey = new NamespacedKey(GardeningTweaks.getInstance(), "FLD");
+    private final FixedMetadataValue ignoredBlockMetaData = new FixedMetadataValue(plugin, "ignored");
     private final HashMap<Integer, Deque<Block>> blockScheduleMap = new HashMap<>();
 
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event) {
-        fastLeafDecay = GardeningTweaks.configManager.getFastLeafDecayConfig();
+        fastLeafDecay = GardeningTweaks.getConfigManager().getFastLeafDecayConfig();
         if (!fastLeafDecay.enabled()) return;
         Block block = event.getBlock();
         if (!(block.getBlockData() instanceof Leaves leaves)) return;
 
-        if (leaves.isPersistent()) return;
+        if (!fastLeafDecay.ignorePersistence() && leaves.isPersistent()) return;
         if (leaves.getDistance() >= 7) {
             int currTick = GardeningTweaks.getCurrentTick();
             if (blockScheduleMap.containsKey(currTick)) {
@@ -40,6 +48,50 @@ public class FastLeafDecay implements Listener {
                 breakLeaves(currTick);
             }
         }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!GardeningTweaks.getConfigManager().getFastLeafDecayConfig().ignorePersistence()) return;
+        Block block = event.getBlock();
+        if (!Tag.LEAVES.isTagged(block.getType())) return;
+        updateLeaf(block.getLocation(), true);
+        block.setMetadata("GT_FLD", ignoredBlockMetaData);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!GardeningTweaks.getConfigManager().getFastLeafDecayConfig().ignorePersistence()) return;
+        Block block = event.getBlock();
+        if (!Tag.LEAVES.isTagged(block.getType())) return;
+        updateLeaf(block.getLocation(), false);
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent event) {
+        if (!GardeningTweaks.getConfigManager().getFastLeafDecayConfig().ignorePersistence()) return;
+        Chunk chunk = event.getChunk();
+        byte[] bytes = chunk.getPersistentDataContainer().get(ignoredKey, PersistentDataType.BYTE_ARRAY);
+        HashSet<ChunkLocation> chunkLocations = deserialize(bytes);
+        chunkLocations.forEach((chunkLocation) -> chunk.getBlock(chunkLocation.chunkX(), chunkLocation.chunkY(), chunkLocation.chunkZ()).setMetadata("GT_FLD", ignoredBlockMetaData));
+    }
+
+    private void updateLeaf(Location location, boolean placed) {
+        Chunk chunk = location.getChunk();
+        Block block = location.getBlock();
+        int chunkRelativeX = block.getX() & 0xF;
+        int chunkRelativeY = block.getY() & 0xFF;
+        int chunkRelativeZ = block.getZ() & 0xF;
+        ChunkLocation chunkLocation = new ChunkLocation(chunkRelativeX, chunkRelativeY, chunkRelativeZ);
+
+        PersistentDataContainer dataContainer = chunk.getPersistentDataContainer();
+
+        byte[] byteArr = dataContainer.get(ignoredKey, PersistentDataType.BYTE_ARRAY);
+        HashSet<ChunkLocation> chunkLocations = deserialize(byteArr);
+        if (placed) chunkLocations.add(chunkLocation);
+        else chunkLocations.remove(chunkLocation);
+
+        dataContainer.set(ignoredKey, PersistentDataType.BYTE_ARRAY, serialize(chunkLocations));
     }
 
     private void breakLeaves(int tick) {
@@ -55,7 +107,9 @@ public class FastLeafDecay implements Listener {
                 Block block = blockSchedule.pop();
                 if (!Tag.LEAVES.isTagged(block.getType())) return;
                 if (!(block.getBlockData() instanceof Leaves leaves)) return;
-                if (leaves.getDistance() <= 7) return;
+                if (leaves.getDistance() < 7) return;
+                List<MetadataValue> metaList = block.getMetadata("GT_FLD");
+                if (metaList.size() > 0) return;
 
                 BlockData blockData = block.getType().createBlockData();
                 Location location = block.getLocation();
@@ -66,4 +120,37 @@ public class FastLeafDecay implements Listener {
             }
         }.runTaskTimer(plugin, 3,3);
     }
+
+    private byte[] serialize(HashSet<ChunkLocation> chunkLocations) {
+        try {
+            ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(byteOut);
+            out.writeObject(chunkLocations);
+            out.close();
+
+            return byteOut.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
+    private HashSet<ChunkLocation> deserialize(byte[] bytes) {
+        try {
+            if (bytes == null) return new HashSet<>();
+            ByteArrayInputStream byteIn = new ByteArrayInputStream(bytes);
+            ObjectInputStream in = new ObjectInputStream(byteIn);
+
+            HashSet<ChunkLocation> chunkLocations = (HashSet<ChunkLocation>) in.readObject();
+
+            in.close();
+
+            return chunkLocations;
+        } catch (IOException | ClassNotFoundException err) {
+            err.printStackTrace();
+            return new HashSet<>();
+        }
+    }
+
+    private record ChunkLocation(int chunkX, int chunkY, int chunkZ) implements Serializable {}
 }

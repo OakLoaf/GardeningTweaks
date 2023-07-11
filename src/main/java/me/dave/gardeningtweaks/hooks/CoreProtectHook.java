7c456a1abd0b5a29ca352b9e3842e810b20bfaf4
BuildTools <cooldaveb@live.co.uk>
2023-07-11 03:07:46 +0100

    Fixed indents null pointer

--- src/main/java/me/dave/gardeningtweaks/hooks/CoreProtectHook.java
+++ src/main/java/me/dave/gardeningtweaks/hooks/CoreProtectHook.java
@@ -1,5 +1,6 @@
 package me.dave.gardeningtweaks.hooks;
 
+import me.dave.gardeningtweaks.GardeningTweaks;
 import net.coreprotect.CoreProtect;
 import net.coreprotect.CoreProtectAPI;
 import org.bukkit.Bukkit;
@@ -17,25 +18,28 @@ public class CoreProtectHook {
 
         // Check that the API is enabled
         CoreProtectAPI coreProtect = ((CoreProtect) plugin).getAPI();
-        if (!coreProtect.isEnabled()) return;
 
         // Check that a compatible version of the API is loaded
-        if (coreProtect.APIVersion() < 9) return;
+        if (coreProtect.APIVersion() < 9) {
+            GardeningTweaks.getInstance().getLogger().warning("Using incompatible version of CoreProtect");
+            return;
+        }
 
         this.coreProtect = coreProtect;
     }
 
     public boolean isBlockNatural(Location location) {
         List<String[]> dataList = coreProtect.performLookup(
-                315360000 /* 10 years in seconds */,
-                null,
-                null,
-                null,
-                null,
-                Arrays.asList(1) /* Checks for place action only */,
-                0,
-                location
+            315360000 /* 10 years in seconds */,
+            null,
+            null,
+            null,
+            null,
+            List.of(1) /* Checks for place action only */,
+            0,
+            location
         );
+        if (dataList == null) return true;
 
         return dataList.isEmpty();
     }

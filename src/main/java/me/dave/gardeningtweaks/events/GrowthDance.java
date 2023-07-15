5d23f7c0293c92dc004dc245088972304d3a5a02
BuildTools <cooldaveb@live.co.uk>
2023-07-15 10:21:54 +0100

    Added PlayerGrowthDance event and more api methods

--- src/main/java/me/dave/gardeningtweaks/events/GrowthDance.java
+++ src/main/java/me/dave/gardeningtweaks/events/GrowthDance.java
@@ -1,11 +1,13 @@
 package me.dave.gardeningtweaks.events;
 
 import me.dave.gardeningtweaks.api.events.CropGrowEvent;
+import me.dave.gardeningtweaks.api.events.PlayerGrowthDanceEvent;
 import me.dave.gardeningtweaks.data.ConfigManager;
 import me.dave.gardeningtweaks.utils.GardeningMode;
 import me.dave.gardeningtweaks.GardeningTweaks;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.entity.Player;
@@ -13,13 +15,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
+import javax.annotation.Nullable;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
 public class GrowthDance implements Listener {
+    private static final Random random = new Random();
     private final GardeningTweaks plugin = GardeningTweaks.getInstance();
-    private final Random random = new Random();
     private final HashSet<UUID> cooldownList = new HashSet<>();
 
     @EventHandler
@@ -30,22 +34,31 @@ public class GrowthDance implements Listener {
         if (!event.isSneaking()) return;
         Player player = event.getPlayer();
         if (cooldownList.contains(player.getUniqueId())) return;
+        if (!GardeningTweaks.callEvent(new PlayerGrowthDanceEvent(player))) return;
         cooldownList.add(player.getUniqueId());
         Bukkit.getScheduler().runTaskLater(plugin, () -> cooldownList.remove(player.getUniqueId()), growthDance.cooldownLength());
-        growCrops(growthDance, player.getLocation());
+        growCrops(player.getLocation(), 0.5, 2, growthDance.blocks());
     }
 
-    private void growCrops(ConfigManager.GrowthDance growthDance, Location location) {
+    public static boolean growCrops(Location location, double chance, int radius) {
+        return growCrops(location, chance, radius, 2, null);
+    }
+
+    public static boolean growCrops(Location location, double chance, int radius, @Nullable List<Material> crops) {
+        return growCrops(location, chance, radius, 2, crops);
+    }
+
+    public static boolean growCrops(Location location, double chance, int radius, int height, @Nullable List<Material> crops) {
         Location currLocation = location.clone();
 
-        for (int indexY = 0; indexY < 2; indexY++) {
-            for (int indexX = -2; indexX < 3; indexX++) {
-                for (int indexZ = -2; indexZ < 3; indexZ++) {
+        for (int indexY = 0; indexY < height; indexY++) {
+            for (int indexX = -radius; indexX <= radius; indexX++) {
+                for (int indexZ = -radius; indexZ <= radius; indexZ++) {
                     Block currBlock = currLocation.clone().add(indexX, indexY, indexZ).getBlock();
 
-                    if (currBlock.getBlockData() instanceof Ageable crop && growthDance.blocks().contains(currBlock.getType())) {
+                    if (currBlock.getBlockData() instanceof Ageable crop && (crops == null || crops.contains(currBlock.getType()))) {
                         if (random.nextBoolean()) {
-                            if (!GardeningTweaks.callEvent(new GrowthDanceCropGrowEvent(currBlock))) return;
+                            if (!GardeningTweaks.callEvent(new CropGrowEvent(currBlock))) continue;
 
                             int newAge = crop.getAge() + random.nextInt(3);
                             int maxAge = crop.getMaximumAge();
@@ -57,5 +70,6 @@ public class GrowthDance implements Listener {
                 }
             }
         }
+        return true;
     }
 }

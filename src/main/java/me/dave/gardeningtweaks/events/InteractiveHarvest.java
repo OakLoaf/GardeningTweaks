1a6633c4cb614234800285ede9ff3d1781e98af2
BuildTools <cooldaveb@live.co.uk>
2023-07-16 06:37:49 +0100

    Added BlockDropItemEvent event call

--- src/main/java/me/dave/gardeningtweaks/events/InteractiveHarvest.java
+++ src/main/java/me/dave/gardeningtweaks/events/InteractiveHarvest.java
@@ -6,18 +6,19 @@ import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Ageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.UUID;
+import java.util.*;
 
 public class InteractiveHarvest implements Listener {
     private final GardeningTweaks plugin = GardeningTweaks.getInstance();
@@ -50,10 +51,18 @@ public class InteractiveHarvest implements Listener {
                 block.setType(material);
                 Location location = block.getLocation();
                 World world = block.getWorld();
+                List<Item> entities = new ArrayList<>();
                 for (ItemStack drop : drops) {
                     if (drop.getType().toString().contains("SEEDS")) drop.setAmount(drop.getAmount() - 1);
-                    world.dropItemNaturally(location.clone().add(0.5, 0.5, 0.5), drop);
+                    entities.add(world.dropItemNaturally(location.clone().add(0.5, 0.5, 0.5), drop));
                 }
+
+                if (!entities.isEmpty()) {
+                    if (!GardeningTweaks.callEvent(new BlockDropItemEvent(block, block.getState(), player, entities))) {
+                        entities.forEach(Entity::remove);
+                    }
+                }
+
 
                 if (GardeningTweaks.protocolLibHook != null) GardeningTweaks.protocolLibHook.armInteractAnimation(player);
                 world.spawnParticle(Particle.BLOCK_DUST, location.clone().add(0.5, 0.5, 0.5), 50, 0.3, 0.3, 0.3, crop);

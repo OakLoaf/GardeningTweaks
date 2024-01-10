2079cc369788f2e7f93ca04a4505f092f6601c4d
BuildTools <cooldaveb@live.co.uk>
2024-01-10 16:11:49 +0000

    Fixed typo

--- src/main/java/me/dave/gardeningtweaks/GardeningTweaks.java
+++ src/main/java/me/dave/gardeningtweaks/GardeningTweaks.java
@@ -11,16 +11,12 @@ import me.dave.gardeningtweaks.listener.GardeningTweaksListener;
 import me.dave.platyutils.PlatyUtils;
 import me.dave.platyutils.hook.Hook;
 import me.dave.platyutils.plugin.SpigotPlugin;
-import net.william278.husktowns.libraries.commons.io.FilenameUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public final class GardeningTweaks extends SpigotPlugin {
@@ -59,7 +55,7 @@ public final class GardeningTweaks extends SpigotPlugin {
             getLogger().info("GardeningTweaks now respects HuskTowns Claims.");
         });
 
-        new GardeningTweaksListener().re
+        new GardeningTweaksListener().registerListeners();
 
         getCommand("gardeningtweaks").setExecutor(new GardeningTweaksCmd());
 

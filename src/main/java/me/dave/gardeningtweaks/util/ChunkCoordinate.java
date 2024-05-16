0f5bf21b6227267ee7ff1a193ec365750c581646
Oak <cooldaveb@live.co.uk>
2024-05-16 15:06:58 +0100

    Converted ChunkCoordinate to record

--- src/main/java/me/dave/gardeningtweaks/util/ChunkCoordinate.java
+++ src/main/java/me/dave/gardeningtweaks/util/ChunkCoordinate.java
@@ -5,28 +5,7 @@ import org.bukkit.World;
 
 import java.util.Objects;
 
-public class ChunkCoordinate {
-    private final World world;
-    private final int x;
-    private final int z;
-
-    public ChunkCoordinate(World world, int x, int z) {
-        this.world = world;
-        this.x = x;
-        this.z = z;
-    }
-
-    public World getWorld() {
-        return world;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getZ() {
-        return z;
-    }
+public record ChunkCoordinate(World world, int x, int z) {
 
     public Chunk getChunk() {
         return world.getChunkAt(x, z);
@@ -44,8 +23,4 @@ public class ChunkCoordinate {
         return x == that.x && z == that.z && Objects.equals(world, that.world);
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(world, x, z);
-    }
 }
